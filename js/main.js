0303c86b77b950e74fccd3784081e1da2c42a9c0
Shadowrunner11 <matematicadivina1@gmail.com>
2022-07-10 00:20:26 -0500

    [UPD] effects

--- js/main.js
+++ js/main.js
@@ -6,4 +6,16 @@ $( document ).ready(()=>{
         $("#down-burger").toggleClass('down-burger-a')
         $("#middle-burger").toggleClass('middle-burger-a')
     })
+    const text = `let myVar = "Hello world"`
+    let cont = 1
+    setInterval(()=>{
+        if(cont<=text.length)
+            $("#hello-world").text(text.slice(0,cont)+"|")
+        else if(cont%2)
+            $("#hello-world").text(text+"    ")
+        else
+            $("#hello-world").text(text+"|")
+        cont++
+        if (cont>text.length+10) cont = 1
+    },300)
 })
