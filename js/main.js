7b971d2136cde1b5bc53556f37cdca6d2c64d469
Shadowrunner11 <matematicadivina1@gmail.com>
2022-07-10 05:13:37 -0500

    [FIX] animation code

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ $( document ).ready(()=>{
         $("#up-burger").toggleClass('up-burger-a')
         $("#down-burger").toggleClass('down-burger-a')
         $("#middle-burger").toggleClass('middle-burger-a')
+        $("#links").slideToggle()     
     })
     const text = `let myVar = "Hello world"`
     let cont = 1
