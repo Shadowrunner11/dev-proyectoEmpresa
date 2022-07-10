fc37a2fa1ae1feb0d616014c082b2b6913cebc48
Shadowrunner11 <matematicadivina1@gmail.com>
2022-07-10 06:12:32 -0500

    [FIX] animation code

--- js/main.js
+++ js/main.js
@@ -7,6 +7,17 @@ $( document ).ready(()=>{
         $("#middle-burger").toggleClass('middle-burger-a')
         $("#links").slideToggle()     
     })
+    const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)")
+    if(darkThemeMq)
+        localStorage.setItem("theme","dark")
+    else
+        $("main, header").toggleClass('light-mode')
+
+    $("#change-theme").click(()=>{
+        $("main, header").toggleClass('light-mode')
+        localStorage.setItem("theme",localStorage.getItem("theme")==='dark' ? "ligth":"dark")
+    })
+
     const text = `let myVar = "Hello world"`
     let cont = 1
     setInterval(()=>{
