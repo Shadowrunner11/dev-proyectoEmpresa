3c0fae1577fc52d7c7a79957c4610fff9c38adeb
Shadowrunner11 <matematicadivina1@gmail.com>
2022-07-11 00:22:43 -0500

    [FIX] theme condition

--- js/theme.js
+++ js/theme.js
@@ -1,10 +1,27 @@
 /* eslint-disable no-undef */
 export const initThemer = () => {
-  const darkThemeMq = window.matchMedia('(prefers-color-scheme: dark)')
-  if (darkThemeMq) { localStorage.setItem('theme', 'dark') } else { $('main, header').toggleClass('light-mode') }
+  const { matches: darkThemeMq } = window.matchMedia('(prefers-color-scheme: dark)')
+  console.log(darkThemeMq)
+  if (localStorage.getItem('theme') === 'dark') {
+    console.log('a')
+    $('main, header').toggleClass('dark-mode')
+  } else if (localStorage.getItem('theme') === 'ligth') {
+    console.log('b')
+    $('main, header').toggleClass('light-mode')
+    localStorage.setItem('theme', 'ligth')
+  } else if (darkThemeMq) {
+    console.log('c')
+    localStorage.setItem('theme', 'dark')
+    $('main, header').toggleClass('dark-mode')
+  } else {
+    console.log('d')
+    $('main, header').toggleClass('light-mode')
+    localStorage.setItem('theme', 'ligth')
+  }
 
   $('#change-theme').click(() => {
     $('main, header').toggleClass('light-mode')
+    $('main, header').toggleClass('dark-mode')
     localStorage.setItem('theme', localStorage.getItem('theme') === 'dark' ? 'ligth' : 'dark')
   })
 }
