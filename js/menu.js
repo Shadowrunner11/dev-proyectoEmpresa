4b07a2cd260ff4a67ead3859ba1d85cf875dc6e6
Antony Palomino <antony.leo12agosto@gmail.com>
2022-07-12 22:52:13 -0500

    Prove: another colors

--- js/menu.js
+++ js/menu.js
@@ -4,3 +4,8 @@ const activeMenu = () => {
     menu.classList.toggle('active')
 }
 menu.addEventListener('click', activeMenu)
+
+const scrollTop = document.querySelector('#scrollTop')
+scrollTop.onclick = () => {
+    window.scroll(0, 0)
+}
